5ee5937f683c6585d7d3a73f8cc53f7ab51b1d5c
Zach Morin <zmorin15@gmail.com>
2022-04-23 11:15:03 -0400

    wip

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+const MAT_MODULES = [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatSelectModule
+];
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, MAT_MODULES],
+    exports: [MAT_MODULES]
+})
+export class MaterialModule {
+    
+}
